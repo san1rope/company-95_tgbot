c0e41fe427965a0866b2683ed2eefba5819e32df
san1rope <valetinles@gmail.com>
2025-03-14 07:56:33 +0200

    ush 15

--- test.py
+++ test.py
@@ -7,16 +7,27 @@ async def main():
     api_key = "sk_test_51QkNgeGdotDTv7HK9xGz5DcuanBmjWZjDGlNSzVH7gSEA23R9s88KnDCNEL0XQFYRBfKuzwJEeydr2DtGhRJuUlq00tDshvYQV"
     stripe.api_key = api_key
 
-    product = stripe.Product.create(name="Toy")
-    price = stripe.Price.create(product=product.id, unit_amount=1000, currency="pln")
-    customer = stripe.Customer.create(name="Maxim Lored", email="valetinles@gmail.com", description="Test customer")
-    invoice = stripe.Invoice.create(
-        customer=customer.id, collection_method="send_invoice", days_until_due=1, description="Invoice for driver"
-    )
-    finalized_invoice = stripe.Invoice.finalize_invoice(invoice=invoice.id)
-    invoice_url = finalized_invoice.hosted_invoice_url
+    # product = stripe.Product.create(name="Toy")
+    # price = stripe.Price.create(product=product.id, unit_amount=1000, currency="pln")
+    # customer = stripe.Customer.create(
+    #     name="Maxim Lored", email="valetinles@gmail.com", description="Test customer"
+    # )
 
-    print(f"invoice_url = {invoice_url}")
+    # invoice = stripe.Invoice.create(
+    #     customer="cus_Rw23Btz6mjqqvt", collection_method="send_invoice", days_until_due=1,
+    #     description="Invoice for driver"
+    # )
+    #
+    # invoice_item = stripe.InvoiceItem.create(
+    #     customer="cus_Rw23Btz6mjqqvt", price="price_1R2AwgGdotDTv7HKa9zvL84o", invoice=invoice.id
+    # )
+    #
+    # finalized_invoice = stripe.Invoice.finalize_invoice(invoice=invoice.id)
+    # invoice_url = finalized_invoice.hosted_invoice_url
+    #
+    # print(f"invoice_url = {invoice_url}")
+
+    print(stripe.Invoice.retrieve("in_1R2AwgGdotDTv7HKkbu46LH4"))
 
 
 if __name__ == "__main__":
